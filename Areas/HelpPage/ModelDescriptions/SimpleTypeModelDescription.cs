b2523e7957644dbb9d3bf9135ace5a213fa10524
DAPR <101226963+DanielPalomino28@users.noreply.github.com>
2023-08-26 19:44:23 -0500

    Agregar archivos de proyecto.

---
+++ Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Servicios_Palomino.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Servicios_Palomino.WebApiApplication" Language="C#" %>
