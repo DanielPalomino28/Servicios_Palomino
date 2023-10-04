5403332e7a55240a93c8f88a9849c1f848ca0985
DAPR <101226963+DanielPalomino28@users.noreply.github.com>
2023-10-04 18:48:20 -0500

    Parte 1 Examen

---
+++ Clases/clsRecarga.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Servicios_Palomino.Models;
+
+namespace Servicios_Palomino.Clases
+{
+    public class clsRecarga
+    {
+        public Recarga recarga { get; set; }
+        private DBExamenEntities dbRecarga = new DBExamenEntities();
+        /*
+        private void CalcularBono()
+        {
+            if (empleado.experiencia < 5)
+            {
+                empleado.bono = 0;
+            }
+            else
+            {
+                empleado.bono = 10;
+            }
+        }
+        */
+        public Recarga Consultar(string documento)
+        {
+            return dbRecarga.Recargas.FirstOrDefault(e => e.Documento == documento);
+        }
+        public string Insertar()
+        {
+            //CalcularBono();
+            try
+            {
+                dbRecarga.Recargas.Add(recarga);
+                dbRecarga.SaveChanges();
+                return "Se grabó la recarga para el cliente con el documento: " + recarga.Documento;
+            }
+            catch(Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+        public string Actualizar()
+        {
+            //CalcularBono();
+            try
+            {
+                //dbSuper.EMPLeadoes.AddOrInsert(empleado);
+                Recarga _recarga = Consultar(recarga.Documento);
+                if (_recarga == null)
+                {
+                    return "La recarga para el documento: " + recarga.Documento + ", no existe en la base de datos";
+                }
+                _recarga.NombreCompleto = recarga.NombreCompleto;
+                _recarga.ValorRecarga = recarga.ValorRecarga;
+                _recarga.ValorIncremento = recarga.ValorIncremento;
+                _recarga.TotalRecarga = recarga.TotalRecarga;
+                dbRecarga.SaveChanges();
+                return "Se actualizó la recarga para el documento: " + recarga.Documento;
+            }
+            catch(Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+        public string Eliminar()
+        {
+            try
+            {
+                Recarga _recarga = Consultar(recarga.Documento);
+                if (_recarga == null)
+                {
+                    return "La recarga para el documento: " + recarga.Documento + ", no existe en la base de datos";
+                }
+                dbRecarga.Recargas.Remove(_recarga);
+                dbRecarga.SaveChanges();
+                return "Se eliminó la recarga en la base de datos, documento: " + recarga.Documento;
+            }
+            catch(Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
---
+++ Controllers/RecargaController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Servicios_Palomino.Models;
+using Servicios_Palomino.Clases;
+using System.Web.Http.Cors;
+
+namespace Servicios_Palomino.Controllers
+{
+    [EnableCors(origins: "http://localhost:59187", headers: "*", methods: "*")]
+    public class RecargaController : ApiController
+    {
+        //Consultar
+        public Recarga Get(string Documento)
+        {
+            clsRecarga _recarga = new clsRecarga();
+            return _recarga.Consultar(Documento);
+        }
+
+        // Insertar
+        public string Post([FromBody] Recarga recarga)
+        {
+            clsRecarga _recarga = new clsRecarga();
+            _recarga.recarga = recarga;
+            return _recarga.Insertar();
+        }
+
+        // Actualizar
+        public string Put([FromBody] Recarga recarga)
+        {
+            clsRecarga _recarga = new clsRecarga();
+            _recarga.recarga = recarga;
+            return _recarga.Actualizar();
+        }
+
+        // Eliminar
+        public string Delete([FromBody] Recarga recarga)
+        {
+            clsRecarga _recarga = new clsRecarga();
+            _recarga.recarga = recarga;
+            return _recarga.Eliminar();
+        }
+    }
+}
